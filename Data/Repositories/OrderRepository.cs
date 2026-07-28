f84579ea2f5015105b55daa9e8f99340cbf4327c
masm007 <marco.salazarmejia03@gmail.com>
2026-07-28 00:13:10 -0500

    feat: OrderRepository

--- Data/Repositories/OrderRepository.cs
+++ Data/Repositories/OrderRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Data.Persistence;
 using Domain.Entity;
 using Domain.Repository;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,35 +18,47 @@ namespace Data.Repositories {
 
         public async Task CreateAsync(OrderEntity entity) {
             if (entity == null) throw new ArgumentNullException();
-            await _context.AddAsync(entity);
+            await _context.Orders.AddAsync(entity);
         }
 
         public Task DeleteAsync(OrderEntity entity) {
-            throw new NotImplementedException();
+            if (entity == null) {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            _context.Orders.Remove(entity);
+            return Task.CompletedTask;
         }
 
-        public Task<IEnumerable<OrderEntity>> GetAllAsync() {
-            throw new NotImplementedException();
+        public async Task<IEnumerable<OrderEntity>> GetAllAsync() {
+            return await _context.Orders.AsNoTracking().Include(ord => ord.OrderDetails)
+                .OrderBy(ord => ord.Id).ToListAsync();
         }
 
-        public Task<IEnumerable<OrderEntity>> GetAllByUserIdAsync(int userId) {
-            throw new NotImplementedException();
+        public async Task<IEnumerable<OrderEntity>> GetAllByUserIdAsync(int userId) {
+            return await _context.Orders.AsNoTracking().Include(ord => ord.OrderDetails)
+                .Where(ord => ord.UserId == userId).OrderBy(ord => ord.Id).ToListAsync();
         }
 
-        public Task<OrderEntity?> GetByIdAsync(int id) {
-            throw new NotImplementedException();
+        public async Task<OrderEntity?> GetByIdAsync(int id) {
+            return await _context.Orders.Include(ord => ord.OrderDetails)
+                .FirstOrDefaultAsync(ord => ord.Id == id);
         }
 
-        public Task<OrderEntity?> GetByOrderNumberAsync(string orderNumber) {
-            throw new NotImplementedException();
+        public async Task<OrderEntity?> GetByOrderNumberAsync(string orderNumber) {
+            return await _context.Orders.Include(ord => ord.OrderDetails)
+                .FirstOrDefaultAsync(ord => ord.OrderNumber == orderNumber);
         }
 
-        public Task<int> SaveChangesAsync() {
-            throw new NotImplementedException();
+        public async Task<int> SaveChangesAsync() {
+            return await _context.SaveChangesAsync();
         }
 
         public Task UpdateAsync(OrderEntity entity) {
-            throw new NotImplementedException();
+            if (entity == null) {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            _context.Orders.Update(entity);
+            return Task.CompletedTask;
         }
     }
 }
